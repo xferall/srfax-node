0f306886d7b7609fabc63e93e58a2e792386d8bc
Greg Coonrod <>
2021-04-30 15:27:16 -0500

    Changing request api to allow for multiple attachements.

--- src/lib/api/queueFax.ts
+++ src/lib/api/queueFax.ts
@@ -1,14 +1,14 @@
-import { QueueFaxParams, QueueFaxResponse } from './types';
+import { QueueFaxResponse } from './types';
 import { getClient } from './client';
 
 /**
  * Submits a fax to the SRFax QUEUE_FAX API
- * @param {QueueFaxParams} params The raw parameters for enqueueing a fax
+ * @param {any} params The raw parameters for enqueueing a fax.
+ *                     These should be a combination of {QueueFaxParams} and
+ *                     any pairs of file names and contents.
  * @returns {Promise<QueueFaxResponse>}
  */
-export async function queueFax(
-  params: QueueFaxParams
-): Promise<QueueFaxResponse> {
+export async function queueFax(params: any): Promise<QueueFaxResponse> {
   const client = getClient();
   const { data } = await client.post<QueueFaxResponse>('', params);
   return data;
